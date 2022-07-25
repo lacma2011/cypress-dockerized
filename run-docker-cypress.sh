12e6ea71b3e69fc6c1a9b1c7fddf5e8d4061b2f5
Jerome Bordallo <lacma2011@github.com>
2022-07-25 11:59:48 -0700

    update

--- run-docker-cypress.sh
+++ run-docker-cypress.sh
@@ -1,2 +1,5 @@
 xhost +"local:docker@"
-UID_="$(id -u)" GID_="$(id -g)" GID_CYPRESS="$(getent group docker | cut -d: -f3)" docker-compose -f "docker-compose-cypress.yml" up
+#UID_="$(id -u)" GID_="$(id -g)" GID_CYPRESS="$(getent group docker | cut -d: -f3)" docker-compose -f "docker-compose-cypress.yml" up
+
+docker-compose up
+
